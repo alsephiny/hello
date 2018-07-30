1cbb7fcca4055e65585e808707a56a092ce6efd7
alsephina <alsephina@sina.com>
2018-07-30 15:54:24 +0800

    xxx

---
+++ fileList.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+
+public class fileList{
+public static void main(String[] args){
+//	for(File f:(new File("e:/")).listFiles((File f)->f.isDirectory())) {
+//		System.out.println(f);
+//	}
+	String dir = "d:/";
+	for(File f:(new File(dir)).listFiles()) {
+		if(f.isDirectory()) System.out.println(f);
+	}
+}}
